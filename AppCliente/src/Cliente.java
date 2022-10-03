dcfa6bc553c0c542e60a786dfad132d5c12e23be
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-10-03 00:25:14 -0500

    logs y el espacio

--- AppCliente/src/Cliente.java
+++ AppCliente/src/Cliente.java
@@ -22,27 +22,27 @@ public class Cliente extends Thread {
         this.id = id;
         this.totalConexiones = totalConexiones;
     }
-    
+
     private static String getFileChecksum(MessageDigest digest, File file) throws IOException
     {
     //Get file input stream for reading the file content
     FileInputStream fis = new FileInputStream(file);
-    
+
     //Create byte array to read data in chunks
     byte[] byteArray = new byte[1024];
-    int bytesCount = 0; 
-        
+    int bytesCount = 0;
+
     //Read file data and update in message digest
     while ((bytesCount = fis.read(byteArray)) != -1) {
         digest.update(byteArray, 0, bytesCount);
     };
-    
+
     //close the stream; We don't need it now.
     fis.close();
-    
+
     //Get the hash's bytes
     byte[] bytes = digest.digest();
-    
+
     //This bytes[] has bytes in decimal format;
     //Convert it to hexadecimal format
     StringBuilder sb = new StringBuilder();
@@ -50,7 +50,7 @@ public class Cliente extends Thread {
     {
         sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
     }
-    
+
     //return complete hash
     return sb.toString();
     }
@@ -62,18 +62,18 @@ public class Cliente extends Thread {
             PrintWriter escritorS = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader lectorS = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             InputStream in = socket.getInputStream();
-            DataInputStream clientData = new DataInputStream(in); 
-            int bytesRead;  
+            DataInputStream clientData = new DataInputStream(in);
+            int bytesRead;
             String mensajeInicio = "Cliente " + id + " intentando conectarse al servidor";
             System.out.println(mensajeInicio);
             escritorS.println(mensajeInicio);
             String conexionE = lectorS.readLine();
-            if (conexionE.equals("Conexion establecida con cliente" + id)) {
+            if (conexionE.equals("Conexion establecida con cliente " + id)) {
                 System.out.println("Cliente " + id + " establecio conexion con el servidor");
                 String peticionArchivo = "Cliente " + id + " solicita tamanio del archivo";
                 System.out.println(peticionArchivo);
                 escritorS.println(peticionArchivo);
-                long size = clientData.readLong(); 
+                long size = clientData.readLong();
                 System.out.println("Cliente " + id + " recibio tamanio archivo" + size);
                 String clienteListo = "Cliente " + id + " listo para recibir archivo";
                 System.out.println(clienteListo);
@@ -81,13 +81,13 @@ public class Cliente extends Thread {
                 Boolean archivoRecepcion = false;
 
                 while (archivoRecepcion == false){
-                    String fileName = "AppCliente/src/ArchivosRecibidos"+id+"-Prueba-"+totalConexiones+".txt" ;  
-                    OutputStream output = new FileOutputStream(fileName); 
-                    byte[] buffer = new byte[1024]; 
-                    while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1)     
-                    {     
-                        output.write(buffer, 0, bytesRead);     
-                        size -= bytesRead;     
+                    String fileName = "AppCliente/src/ArchivosRecibidos"+id+"-Prueba-"+totalConexiones+".txt" ;
+                    OutputStream output = new FileOutputStream(fileName);
+                    byte[] buffer = new byte[1024];
+                    while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1)
+                    {
+                        output.write(buffer, 0, bytesRead);
+                        size -= bytesRead;
                     }
                     String archivoRecibido = "Cliente " + id + " solicita hash";
                     System.out.println(archivoRecibido);
@@ -103,7 +103,7 @@ public class Cliente extends Thread {
                         System.out.println(archivoInCorrecto);
                         escritorS.println(archivoInCorrecto);
                     }
-                    output.close();   
+                    output.close();
                 }
                 String archivoCorrecto = "Cliente " + id + " recibo archivo correcto";
                 System.out.println(archivoCorrecto);
